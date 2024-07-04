44b30f5fc99e99f2c7dec3c690eeecf7676d6ba6
Fernando Rosa <90939021+fernandoerosa@users.noreply.github.com>
2024-07-03 21:38:19 -0300

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IonToggle, IonIcon, IonText, IonButton } from '@ionic/angular/standalone';
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports:[IonToggle, IonIcon, IonText, IonButton]
+  imports:[IonToggle, IonIcon, IonText, IonButton, RouterLink]
 })
 export class HeaderComponent  implements OnInit {
 
